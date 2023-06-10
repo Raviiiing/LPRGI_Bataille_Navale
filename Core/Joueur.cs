70d510c5c65beeb1bcd4270946c83cd3f3224f6e
Lucas Ferreira <65078868+LucasFrra@users.noreply.github.com>
2023-06-10 15:38:37 +0200

    Revert "modification Jeu.cs (public) et Joueur.cs (jsp ?), ajout de interfaceG, ma solution pour MAUI XAML."
    This reverts commit 911548e787046ff53db6bbfb4b8ad287f830c1f5.

--- Core/Joueur.cs
+++ Core/Joueur.cs
@@ -1,16 +1,16 @@
-    namespace Core;
+namespace Core;
 
-    public class Joueur
+public class Joueur
+{
+    public Joueur(string nom, Cellule[,] grille)
     {
-        public Joueur(string nom, Cellule[,] grille)
-        {
-            Nom = nom;
-            Grille = grille;
-            Bateaux = new List<Bateau>();
-        }
-
-        public string Nom { get; set; }
-        public Cellule[,] Grille { get; set; }
-        public List<Bateau> Bateaux { get; set; }
-        public bool aGagne { get; set; } = false;
+        Nom = nom;
+        Grille = grille;
+        Bateaux = new List<Bateau>();
     }
+
+    public string Nom { get; set; }
+    public Cellule[,] Grille { get; set; }
+    public List<Bateau> Bateaux { get; set; }
+    public bool aGagne { get; set; } = false;
+}
